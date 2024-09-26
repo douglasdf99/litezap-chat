b4c0a7bcd39eb3b65b8876944dd14ebd47939eef
Douglas Matos <douglas.nascimento@hotmart.com>
2024-09-25 22:03:19 -0300

    fix: verify message

--- backend/src/services/TicketServices/UpdateTicketService.ts
+++ backend/src/services/TicketServices/UpdateTicketService.ts
@@ -11,12 +11,12 @@ import ShowWhatsAppService from "../WhatsappService/ShowWhatsAppService";
 import SendWhatsAppMessage from "../WbotServices/SendWhatsAppMessage";
 import FindOrCreateATicketTrakingService from "./FindOrCreateATicketTrakingService";
 import GetTicketWbot from "../../helpers/GetTicketWbot";
-import { verifyMessage } from "../WbotServices/wbotMessageListener";
 import ListSettingsServiceOne from "../SettingServices/ListSettingsServiceOne"; //NOVO PLW DESIGN//
 import ShowUserService from "../UserServices/ShowUserService"; //NOVO PLW DESIGN//
 import { isNil } from "lodash";
 import Whatsapp from "../../models/Whatsapp";
 import { log } from 'console';
+import verifyMessage from "../WbotServices/verifyMessage";
 
 interface TicketData {
   status?: string;
